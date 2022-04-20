61906c66eeb0bb40f3e52555c63c323242c7845d
Gianella_Coronel <u201919040@upc.edu.pe>
2022-04-20 18:48:19 -0500

    update US05

--- src/test/java/com/appdhome/controller/AppointmentControllerTest.java
+++ src/test/java/com/appdhome/controller/AppointmentControllerTest.java
@@ -1,9 +1,6 @@
 package com.appdhome.controller;
 //US05: Como cliente quiero contratar a un trabajador para arreglar el desperfecto de mi hogar.
-import com.appdhome.entities.Appointment;
-import com.appdhome.entities.Customer;
-import com.appdhome.entities.Employee;
-import com.appdhome.entities.PaymentMethod;
+import com.appdhome.entities.*;
 import com.appdhome.services.impl.AppointmentServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,16 +29,23 @@ public class AppointmentControllerTest {
 
     private List<Appointment> appointmentList;
     Date dateD = new Date();
-    Customer customer = new Customer();
+    Account account = new Account();
+    District district = new District();
+    Customer customer = new Customer(1L, "Milagros", "Sotomayor", "78478541",
+                                 "mili@hotmail.com", "998745247", account, district);
     Employee employee = new Employee();
     PaymentMethod paymentMethod = new PaymentMethod();
+
+    Appointment appointment = new Appointment();
+
     // carga de data
     @BeforeEach
     void setUp(){
         appointmentList=new ArrayList<>();
         appointmentList.add(new Appointment(1L, "pendiente", "desperfecto en mi cocina, no prenden las hornillas",
                 dateD, "Avenida Abancay", 5, customer, employee, paymentMethod));
-
+        appointmentList.add(new Appointment(2L, "pendiente", "desperfecto en mi lavadora, hace ruido",
+                dateD, "Avenida Aviación", 4, customer, employee, paymentMethod));
     }
 
     @Test
@@ -49,4 +54,11 @@ public class AppointmentControllerTest {
         mockMvc.perform(get("/api/appointments")).andExpect(status().isOk());
     }
 
+    @Test
+    void CheckCustomer() throws Exception{
+        List<Appointment> appointmentListP = appointmentService.findByIdCustomer(1L);
+        if (appointmentList.get(1) == appointmentListP){
+            mockMvc.perform(get("/api/appointments/searchByIdCustomer/1")).andExpect(status().isOk());
+        }
+    }
 }
