22bec2f7fba43384fa0cabbbcdded28fd83578db
App_moviles <u201913741@upc.edu.pe>
2022-04-19 23:55:30 -0500

    Test US04 US14

---
+++ src/test/java/com/appdhome/controller/AccountControllerTest.java
@@ -0,0 +1,53 @@
+package com.appdhome.controller;
+
+import com.appdhome.entities.Account;
+
+import com.appdhome.services.impl.AccountServiceImpl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(controllers=AccountController.class)
+@ActiveProfiles("test")
+
+public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private AccountServiceImpl accountService;
+
+    private List<Account> accountList;
+
+
+
+
+    @BeforeEach
+    void setUp(){
+        accountList=new ArrayList<>();
+        accountList.add(new Account(1L, "Katerin", "123" , 1,true ));
+        accountList.add(new Account(1L, "Gianella", "1234" , 1,true ));
+        accountList.add(new Account(1L, "Jair", "12345" , 1,true ));
+
+    }
+    @Test
+    void ValidateAccount()throws Exception{
+        given(accountService.getAll()).willReturn(accountList);
+        mockMvc.perform(get("/api/auth/account"));
+
+    }
+
+}
---
+++ src/test/java/com/appdhome/controller/EmployeeControllerTest.java
@@ -0,0 +1,52 @@
+package com.appdhome.controller;
+
+import com.appdhome.entities.Account;
+import com.appdhome.entities.District;
+import com.appdhome.entities.Employee;
+import com.appdhome.entities.Specialty;
+import com.appdhome.services.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest(controllers=EmployeeController.class)
+@ActiveProfiles("test")
+
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private EmployeeServiceImpl employeeService;
+    private List<Employee> employeeList;
+
+    Account account= new Account();
+    Specialty specialty=new Specialty();
+    District district= new District();
+
+    @BeforeEach
+    void SetUp(){
+        employeeList=new ArrayList<>();
+        employeeList.add(new Employee(2L ,"Gianella","Coronel","7236238","928392","giane@gmail.com","1-1-2002",account,specialty,district));
+    }
+
+    @Test
+    void CheckCustomers() throws Exception{
+        given(employeeService.getAll()).willReturn(employeeList);
+        mockMvc.perform(get("/api/employees")).andExpect(status().isOk());
+    }
+
+
+}
+
